6c7f8b23e9ad40de92f67324ca72e8564968aaf9
Solidus756 <deniscouchard@gmail.com>
2025-02-09 22:14:17 +0100

    Update views.py

--- charity_streaming/views.py
+++ charity_streaming/views.py
@@ -1,5 +1,11 @@
 # charity_streaming/views.py
 from django.shortcuts import render
+from accounts.forms import StreamerForm, StaffForm
 
 def home(request):
-    return render(request, 'home.html')
+    streamer_form = StreamerForm()
+    staff_form = StaffForm()
+    return render(request, 'home.html', {
+        'streamer_form': streamer_form,
+        'staff_form': staff_form,
+    })
