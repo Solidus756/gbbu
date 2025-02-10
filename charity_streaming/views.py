cdb48c0f981aefbbc16f812e0ea12d526e581e38
Solidus756 <deniscouchard@gmail.com>
2025-02-10 21:16:50 +0100

    Update views.py

--- charity_streaming/views.py
+++ charity_streaming/views.py
@@ -5,7 +5,9 @@ from accounts.forms import StreamerForm, StaffForm
 def home(request):
     streamer_form = StreamerForm()
     staff_form = StaffForm()
+    social_formset = SocialAccountFormSet(prefix="socialaccount_set")
     return render(request, 'home.html', {
         'streamer_form': streamer_form,
         'staff_form': staff_form,
+        'social_formset': social_formset,
     })
