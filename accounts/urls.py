f690071fb5ae216ef6cf8d3da21acf14f155b0c7
Solidus756 <deniscouchard@gmail.com>
2025-02-10 22:23:13 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.public_registration, name='public_registration'),
     path('register/streamer/', views.register_streamer, name='register_streamer'),
-    path('register/staff/', views.register_staff, name='register_staff'),
+    #path('register/staff/', views.register_staff, name='register_staff'),
     path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),
     path('dashboard/', views.user_dashboard, name='dashboard'),
