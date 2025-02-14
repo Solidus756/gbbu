cf3c90d0e81f00d840ecc66b28e602397df2a89a
Solidus756 <deniscouchard@gmail.com>
2025-02-14 13:40:08 +0100

    Update settings.py

--- charity_streaming/settings.py
+++ charity_streaming/settings.py
@@ -92,7 +92,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'static_collected'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
